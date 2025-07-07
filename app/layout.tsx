93b63db854ae63956b3c26d555a54eb51b814625
nick-jy-huang <ae8602563@gmail.com>
2025-07-07 21:44:34 +0800

    fix: meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,34 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import '@/styles/globals.css';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Quotation Form',
-  description: 'Make quotations easily.',
+  title: "Quotation Form",
+  description: "Make quotations easily.",
+  icons: {
+    icon: "/favicon.png",
+  },
+  openGraph: {
+    title: "Quotation Form",
+    description: "Make quotations easily.",
+    images: [
+      {
+        url: "https://quotation-app-zeta.vercel.app/",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -25,7 +39,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" sizes="any" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
@@ -33,11 +46,16 @@ export default function RootLayout({
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
-        {process.env.NODE_ENV === 'development' && (
-          <script crossOrigin="anonymous" src="//unpkg.com/react-scan/dist/auto.global.js" />
+        {process.env.NODE_ENV === "development" && (
+          <script
+            crossOrigin="anonymous"
+            src="//unpkg.com/react-scan/dist/auto.global.js"
+          />
         )}
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} bg-grid flex min-h-screen flex-col bg-gray-100`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} bg-grid flex min-h-screen flex-col bg-gray-100`}
+      >
         <main>{children}</main>
       </body>
     </html>
