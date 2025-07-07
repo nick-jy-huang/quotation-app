97ddf0a03155302e5bda1629570cd0999831d2dc
nick-jy-huang <ae8602563@gmail.com>
2025-07-08 00:37:05 +0800

    feat: axe-core

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import QuotationForm from '@/components/QuotationForm';
-import QuotationPreview from '@/components/QuotationPreview';
-import Button from '@/components/prototype/Button';
+import { useState } from "react";
+import QuotationForm from "@/components/QuotationForm";
+import QuotationPreview from "@/components/QuotationPreview";
+import Button from "@/components/prototype/Button";
+import { useEffect } from "react";
 
-type EDIT_TYPES = 'edit' | 'preview';
+type EDIT_TYPES = "edit" | "preview";
 
 export default function Home() {
-  const [activeTab, setActiveTab] = useState<EDIT_TYPES>('edit');
+  const [activeTab, setActiveTab] = useState<EDIT_TYPES>("edit");
 
   const renderConponent = {
     edit: <QuotationForm />,
     preview: <QuotationPreview />,
   };
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "development") {
+      import("@/utils/axe");
+    }
+  }, []);
 
   return (
     <div className="flex h-screen flex-col">
@@ -22,21 +29,23 @@ export default function Home() {
           <div className="flex items-center justify-between py-4">
             <div className="flex gap-4">
               <img src="/favicon.png" alt="logo" className="h-8 w-8" />
-              <h1 className="text-2xl font-bold text-gray-900">Quotation Form</h1>
+              <h1 className="text-2xl font-bold text-gray-900">
+                Quotation Form
+              </h1>
             </div>
 
             <div className="hidden space-x-4 sm:flex">
               <Button
-                onClick={() => setActiveTab('edit')}
-                variant={activeTab === 'edit' ? 'primary' : 'secondary'}
+                onClick={() => setActiveTab("edit")}
+                variant={activeTab === "edit" ? "primary" : "secondary"}
                 className="gap-2"
               >
                 <i className="fa-solid fa-pen-to-square"></i>
                 編輯報價單
               </Button>
               <Button
-                onClick={() => setActiveTab('preview')}
-                variant={activeTab === 'preview' ? 'primary' : 'secondary'}
+                onClick={() => setActiveTab("preview")}
+                variant={activeTab === "preview" ? "primary" : "secondary"}
                 className="gap-2"
               >
                 <i className="fa-solid fa-eye"></i>
@@ -47,7 +56,7 @@ export default function Home() {
               <select
                 className="block w-32 rounded-md border border-gray-300 bg-white py-2 pr-0 pl-2 text-sm text-gray-900 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none"
                 value={activeTab}
-                onChange={e => setActiveTab(e.target.value as EDIT_TYPES)}
+                onChange={(e) => setActiveTab(e.target.value as EDIT_TYPES)}
                 aria-label="切換頁籤"
               >
                 <option value="edit">編輯報價單</option>
@@ -59,11 +68,16 @@ export default function Home() {
       </header>
 
       <main className="flex-1 overflow-y-auto">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">{renderConponent[activeTab]}</div>
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          {renderConponent[activeTab]}
+        </div>
       </main>
 
       <footer className="sticky bottom-0 z-10 w-full space-x-2 bg-white py-4 text-center text-xs text-gray-500">
-        <span>&copy; {new Date().getFullYear()} Quotation Form For. All rights reserved.</span>
+        <span>
+          &copy; {new Date().getFullYear()} Quotation Form For. All rights
+          reserved.
+        </span>
         <span className="mt-2 text-xs text-gray-400">
           All Icons by&nbsp;
           <a
---
+++ utils/axe.ts
@@ -0,0 +1,17 @@
+import axe from "axe-core";
+
+if (typeof window === "undefined" || typeof document === "undefined") {
+  throw new Error("axe-core can only be run in the browser.");
+}
+axe
+  .run()
+  .then((results) => {
+    if (results.violations.length) {
+      console.warn("Accessibility issues found:", results.violations);
+    } else {
+      console.log("No accessibility issues found!");
+    }
+  })
+  .catch((err) => {
+    console.error("Something bad happened:", err.message);
+  });
